f700bc95442bd46d001ac1286ba400872e440d2d
KovDan8757 <62889708+KovDan8757@users.noreply.github.com>
2021-03-18 15:59:34 +0100

    Update lb-check.sh

--- lb-check.sh
+++ lb-check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Eltávolítom a korábbi logokat
+
 rm curl.log
 rm count.log
 
-# Egy while ciklussal 100x meg"curlingelem" az LB-et, és a válaszokat lementem egy logba
+
 load=1
 
 while [ $load -le 30 ]
@@ -14,18 +14,16 @@ do
   echo $load
 done
 
-# az ETagokat vágom ki a logból, ez tartalmazza a serverek egyedi instance-ID-ját és ezek alapján számolok
 grep 'ETag' curl.log | cut -d ":" -f 2 | sort | uniq -c >> count.log
 
-# változókba mentem a szétbontott count.log -ot
 valasz=$( cat count.log | cut -d '"' -f 1 )
 server=$( cat count.log | cut -d '"' -f 2 )
 
-# ezeket a változókat tömbé formálom, hogy külön-külön hozzáférjek az adatokhoz
+
 v=($valasz)
 s=($server)
 
-# visszaadja a tömb méretét
+
 y=${#v[@]}
 
 echo "-----------------------------------------------------------"
@@ -33,8 +31,6 @@ echo $y "Servers we have behind the Load-Balancer."
 echo "-----------------------------------------------------------"
 
 i=0
-
-#a szétbontott és mentett adatokat összetársítva kiiratom.
 
 for i in ${!s[@]}; do
       echo "ServerID: "${s[$i]}",  Give it answers:" ${v[$i]}
